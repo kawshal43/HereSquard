a9b24c0081635d9c1dbf5797d468d2a65accb650
kawshal <111242719+kawshal43@users.noreply.github.com>
2025-04-03 01:34:46 +0530

    init commit

---
+++ src/test/java/com/example/herosquard/HeroSquardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herosquard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroSquardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
